a8998eb56adfd283e5ca2bd3f54bfffdf6347b4c
scresh <screshowski@gmail.com>
2019-11-02 09:57:22 +0100

    Split code into functions in file_operations package

--- file_operations/reader.py
+++ file_operations/reader.py
@@ -8,26 +8,44 @@ class FileReader:
     def _read_int(self, precision):
         return int.from_bytes(self.file.read(precision), byteorder='big')
 
-    def read(self):
+    def _read_symbols(self):
         symbol_count = self._read_int(1) + 1
         symbols = [self._read_int(1) for _ in range(symbol_count)]
+        return symbols
 
+    def _read_ranges(self, symbol_count):
         symbol_range_precision = self._read_int(1)
         denominator = 2 ** (8 * symbol_range_precision)
 
         numerators = [self._read_int(symbol_range_precision) for _ in range(symbol_count)]
         ranges = [Fraction(numerator, denominator) for numerator in numerators] + [Fraction(1, 1)]
+        return ranges
 
-        symbol_ranges = {symbols[i]: (ranges[i], ranges[i+1]) for i in range(symbol_count)}
+    def _read_content_length(self):
+        content_length_precision = self._read_int(1)
+        content_length = self._read_int(content_length_precision)
+        return content_length
 
-        length_precision = self._read_int(1)
-        length = self._read_int(length_precision)
-
+    def _read_content_fraction(self):
         numerator_precision_precision = self._read_int(1)
         numerator_precision = self._read_int(numerator_precision_precision)
         numerator_normalized = self._read_int(numerator_precision)
 
         content_fraction = Fraction(numerator_normalized, 2 ** (8 * numerator_precision))
+        return content_fraction
 
+    @staticmethod
+    def get_symbols_ranges(symbols, ranges):
+        symbol_count = len(symbols)
+        symbol_ranges = {symbols[i]: (ranges[i], ranges[i + 1]) for i in range(symbol_count)}
+        return symbol_ranges
+
+    def read(self):
+        symbols = self._read_symbols()
+        ranges = self._read_ranges(symbol_count=len(symbols))
+        symbols_ranges = self.get_symbols_ranges(symbols, ranges)
+        content_length = self._read_content_length()
+        content_fraction = self._read_content_fraction()
         self.file.close()
-        return content_fraction, length, symbol_ranges
+
+        return content_fraction, content_length, symbols_ranges
--- file_operations/writer.py
+++ file_operations/writer.py
@@ -6,43 +6,52 @@ class FileWriter:
         self.file = open(filename, 'wb+')
 
     @staticmethod
-    def get_normalized_fraction(fraction, precision):
+    def _get_normalized_fraction(fraction, precision):
         max_number = 2 ** (8 * precision)
         return int(fraction * max_number)
 
     @staticmethod
-    def get_precision(number):
+    def _get_precision(number):
         return math.ceil(math.log2(number + 1) / 8)
 
-    def write_int(self, number_int, precision):
+    def _write_int(self, number_int, precision):
         number_bytes = number_int.to_bytes(precision, byteorder='big')
         self.file.write(number_bytes)
 
-    def write(self, content_fraction, length, symbols_ranges):
+    def _write_symbols(self, symbols_ranges):
         symbol_count = len(symbols_ranges)
-        self.write_int(symbol_count - 1, 1)
+        self._write_int(symbol_count - 1, 1)
 
         for symbol in symbols_ranges.keys():
-            self.write_int(symbol, 1)
+            self._write_int(symbol, 1)
 
+    def _write_ranges(self, symbols_ranges):
         ranges_start = [x[0] for x in symbols_ranges.values()]
-        symbol_range_precision = self.get_precision((1 - ranges_start[-1]).denominator)
-        self.write_int(symbol_range_precision, 1)
+        symbol_range_precision = self._get_precision((1 - ranges_start[-1]).denominator)
+        self._write_int(symbol_range_precision, 1)
 
         for range_start in ranges_start:
-            range_start_normalized = self.get_normalized_fraction(range_start, symbol_range_precision)
-            self.write_int(range_start_normalized, symbol_range_precision)
+            range_start_normalized = self._get_normalized_fraction(range_start, symbol_range_precision)
+            self._write_int(range_start_normalized, symbol_range_precision)
 
-        length_precision = self.get_precision(length)
-        self.write_int(length_precision, 1)
-        self.write_int(length, length_precision)
+    def _write_content_length(self, content_length):
+        content_length_precision = self._get_precision(content_length)
+        self._write_int(content_length_precision, 1)
+        self._write_int(content_length, content_length_precision)
 
+    def _write_content_fraction(self, content_fraction):
         numerator = content_fraction.numerator
-        numerator_precision = self.get_precision(numerator) + 1
-        numerator_precision_precision = self.get_precision(numerator_precision)
-        numerator_normalized = self.get_normalized_fraction(content_fraction, numerator_precision)
+        numerator_precision = self._get_precision(numerator) + 1
+        numerator_precision_precision = self._get_precision(numerator_precision)
+        numerator_normalized = self._get_normalized_fraction(content_fraction, numerator_precision)
 
-        self.write_int(numerator_precision_precision, 1)
-        self.write_int(numerator_precision, numerator_precision_precision)
-        self.write_int(numerator_normalized, numerator_precision)
+        self._write_int(numerator_precision_precision, 1)
+        self._write_int(numerator_precision, numerator_precision_precision)
+        self._write_int(numerator_normalized, numerator_precision)
+
+    def write(self, content_fraction, content_length, symbols_ranges):
+        self._write_symbols(symbols_ranges)
+        self._write_ranges(symbols_ranges)
+        self._write_content_length(content_length)
+        self._write_content_fraction(content_fraction)
         self.file.close()
